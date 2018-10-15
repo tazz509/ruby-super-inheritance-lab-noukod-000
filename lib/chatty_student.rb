09f9fc5d229fa5c74e4ec7160aae750dc15b3c89
carly <carly@noukod.com>
2018-10-15 19:19:32 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,7 +6,15 @@ class ChattyStudent < Student
   end
 
   def raise_hand
-    10.times do
+    "super"
+      "super"
+      "super"
+      "super"
+      "super"
+      "super"
+      "super"
+      "super"
+      "super"
       "super"
     end
   end
